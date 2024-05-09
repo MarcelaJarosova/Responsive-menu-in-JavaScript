dfd1a77972e9e3f68cea4b4988ea4b25fab4f9f9
MarcelaJarosova <162338677+MarcelaJarosova@users.noreply.github.com>
2024-05-09 17:50:25 +0200

    Add files via upload

---
+++ Responsive menu in JavaScript/script.js
@@ -0,0 +1,16 @@
+const menuIcon = document.querySelector('.menu-icon')
+const menuList = document.querySelector('nav')
+const hamIcon = document.querySelector('.fa-solid')
+
+menuIcon.addEventListener('click', () => {
+   if (hamIcon.classList[1] === 'fa-bars') {
+    hamIcon.classList.add('fa-xmark')
+    hamIcon.classList.remove('fa-bars')
+    menuList.style.display = 'block'
+   } else {
+    hamIcon.classList.add("fa-bars")
+    hamIcon.classList.remove("fa-xmark")
+    menuList.style.display = 'none'
+   }
+})
+
